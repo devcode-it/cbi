5bcdf9d659e0b4171355e8959838572119d7469d
itajackass <lodirizzini.giuseppe@gmail.com>
2022-11-15 11:21:54 +0100

    Add files via upload

--- src/RiBa/Records/RecordIB.php
+++ src/RiBa/Records/RecordIB.php
@@ -14,7 +14,7 @@ use DevCode\CBI\BaseRecord;
  * @property string campo_a_disposizione Campo a disposizione dell'Azienda codice_sia_mittente.
  * @property string tipo_flusso Assume il valore: "1" = operazioni generate nell’ambito di attività Market Place.
  * @property string qualificatore_flusso Assume il valore fisso "$".
- * @property string soggetto_veicolare Se i due campi tipo_flusso e qualificatore_flusso sono valorizzati con i valori previsti, deve essere indicato il codice ABI della Banca Gateway MP.
+ * @property string soggetto_veicolatore Se i due campi tipo_flusso e qualificatore_flusso sono valorizzati con i valori previsti, deve essere indicato il codice ABI della Banca Gateway MP.
  * @property string codice_divisa Assume il valore fisso "E" (Euro).
  * @property string centro_applicativo Questo campo è di interesse soltanto della tratta tra Centri Applicativi. Codice ABI del Centro Applicativo destinatario del supporto.
  */
@@ -54,10 +54,9 @@ class RecordIB extends BaseRecord
         'qualificatore_flusso' => [
             'inizio' => 106,
             'dimensione' => 1,
-            'tipo' => 'constant',
-            'valore' => '$',
+            'tipo' => 'string',
         ],
-        'soggetto_veicolare' => [
+        'soggetto_veicolatore' => [
             'inizio' => 107,
             'dimensione' => 5,
             'tipo' => 'string',
